cab6e6f4161a51ad61f39fa73eca3f929590e384
malexanderlim <malexanderlim@gmail.com>
2019-09-19 15:53:06 -0700

    Figured out the loop for guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -9,14 +9,19 @@ echo "Type in a number and then press enter."
 read number
 wordcount=$(ls -l | grep -v ^1 | wc -l)
 echo "You entered: $number."
+until [[ $number -eq $wordcount ]]
+	do
 	if [[ $number -lt $wordcount ]]
 	then 
 		echo "Sorry, too low! Please try again."
+read number
 	fi
 	if [[ $number -gt $wordcount ]]
 	then
 		echo "Sorry, too high! Please try again."
+read number
 	fi
+done
 	if [[ $number -eq $wordcount ]]
 	then
 		echo "Congratulations! You guessed the right number."
